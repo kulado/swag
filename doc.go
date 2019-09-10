47d724e3b02b1b2823ef36632dcba3b331a85304
Kulado Inc <4583496-onedomain@users.noreply.gitlab.com>
2019-09-10 02:39:37 -0600

    updated

---
+++ doc.go
@@ -0,0 +1,5 @@
+/*
+Package swag converts Go annotations to Swagger Documentation 2.0.
+See https://github.com/kulado/swag for more information about swag.
+*/
+package swag // import "github.com/kulado/swag"
